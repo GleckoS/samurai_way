6b4fb1dcb3c3f38f02c20b77491479ecd1d029bd
Glecko <shevabogdan16@gmail.com>
2020-09-19 12:48:07 +0200

    before fetch

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,18 +1,74 @@
-import React from "react";
+import React, {Component} from "react";
 
-const Users = (props) => {
-    debugger
-    return (
-        <div>
-            {props.users.map(u => <div key={u.id}>
+
+export default class Users extends Component {
+
+
+    render() {
+
+        if (this.props.users.length === 0) {
+
+
+            debugger
+
+            this.props.setUsers([{
+                id: 1,
+                photoUrl: `https://blachymaxsystem.pl/wp-content/uploads/2019/01/unknown-user.png`,
+                followed: false,
+                fullName: "Bohdan",
+                status: "HAVAHEYO",
+                location: {city: `Krakow`, country: `Poland`}
+            },
+                {
+                    id: 2,
+                    photoUrl: `https://blachymaxsystem.pl/wp-content/uploads/2019/01/unknown-user.png`,
+                    followed: true,
+                    fullName: "Ehor",
+                    status: "HAEYO",
+                    location: {city: `Odessa`, country: `Ukraine`}
+                },
+                {
+                    id: 3,
+                    photoUrl: `https://blachymaxsystem.pl/wp-content/uploads/2019/01/unknown-user.png`,
+                    followed: false,
+                    fullName: "Dima",
+                    status: "HAVAHYO",
+                    location: {city: `Minsk`, country: `Belarus`}
+                },
+                {
+                    id: 4,
+                    photoUrl: `https://blachymaxsystem.pl/wp-content/uploads/2019/01/unknown-user.png`,
+                    followed: false,
+                    fullName: "Anton",
+                    status: "HAVAHE",
+                    location: {city: `Moscow`, country: `Russia`}
+                },
+                {
+                    id: 5,
+                    photoUrl: `https://blachymaxsystem.pl/wp-content/uploads/2019/01/unknown-user.png`,
+                    followed: false,
+                    fullName: "Roma",
+                    status: "HHEYO",
+                    location: {city: `Tokio`, country: `Japan`}
+                }])
+        }
+
+        return (
+            <div>
+
+                {this.props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
                         <img src={u.photoUrl}/>
                     </div>
                     <div>
                         {u.followed
-                            ? <button onClick={() => {props.unFollow(u.id)}} >Follow</button>
-                            : <button onClick={() => {props.follows(u.id)}}>UnFollow</button>}
+                            ? <button onClick={() => {
+                                this.props.unFollow(u.id)
+                            }}>Follow</button>
+                            : <button onClick={() => {
+                                this.props.follows(u.id)
+                            }}>UnFollow</button>}
                     </div>
                 </span>
                 <span>
@@ -33,10 +89,14 @@ const Users = (props) => {
                         </div>
                     </span>
                 </span>
-            </div>)
-        },
-        </div>
-    )
+                </div>)},
+
+            </div>
+        )
+
+    }
 }
 
-export default Users
+
+
+
